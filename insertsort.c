2d1cd78d7bed43c2e05d62ac7ebbd62abf6b525b
adithyareddych <64892286+adithyareddych@users.noreply.github.com>
2020-10-02 12:16:49 +0530

    Add files via upload
    Added insertion sort

---
+++ insertsort.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+void main(){
+	int arr[100],n,temp,j,i;
+	scanf("%d",&n);
+	for(i=0;i<n;i++){
+		scanf("%d",&arr[i]);
+	}
+	for(i=1;i<n;i++){
+		temp=arr[i];
+		j=i-1;
+		while(j>=0 && arr[j]<temp){
+			arr[j+1]=arr[j];
+		//	arr[j+1]=arr[j];
+			j--;
+			//printf("%d",arr[i]);
+		
+		}
+		arr[j+1]=temp;
+		
+	}
+	for(i=0;i<n;i++){
+		printf("%d",arr[i]);
+	}
+}
